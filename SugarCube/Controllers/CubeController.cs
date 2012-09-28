922b5e6a51ed2c2d69c53590f45a00f3e56d375a
Alfred Chan <alfred_c@hotmail.com>
2012-09-27 21:53:29 -0400

    initial commit

---
+++ SugarCube/Controllers/CubeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SugarCube.Models;
+
+namespace SugarCube.Controllers
+{
+    public class CubeController : Controller
+    {
+        //
+        // GET: /Cube/
+
+        public ActionResult Index()
+        {
+            List<JournalEntry> cubes = new List<JournalEntry>(){
+                new JournalEntry{ Title = "Hello World 1", Date=DateTime.Today, Text = "This is a short message.", Type="journal" },
+                new JournalEntry{ Title = "Hello World 2", Date=DateTime.Today, Text = "This is a somewhat longer message.", Type="other" },
+                new JournalEntry{ Title = "Hello World 3", Date=DateTime.Today, Text = "This is a message with an image.", Image = "~/Content/Images/logo.png", Type="journal"},
+                new JournalEntry{ Title = "Hello World 4", Date=DateTime.Today, Image = "~/Content/Images/logo.png", Type="journal" },
+                new JournalEntry{ Title = "Hello World 5", Date=DateTime.Today, Text = "This is a short message.", Type="other" },
+                new JournalEntry{ Title = "Hello World 6", Date=DateTime.Today, Text = "This is a somewhat longer message.", Type="journal" },
+                new JournalEntry{ Title = "Hello World 7", Date=DateTime.Today, Image = "~/Content/Images/logo.png", Type="journal" },
+                new JournalEntry{ Title = "Hello World 8", Date=DateTime.Today, Text = "This is a short message.", Type="other" },
+                new JournalEntry{ Title = "Hello World 9", Date=DateTime.Today, Text = "This is a somewhat longer message.", Type="journal" },
+                new JournalEntry{ Title = "Hello World 10", Date=DateTime.Today, Text = "This is a short message.", Type="journal" },
+                new JournalEntry{ Title = "Hello World 11", Date=DateTime.Today, Text = "This is a somewhat longer message.", Type="other" },
+                new JournalEntry{ Title = "Hello World 12", Date=DateTime.Today, Text = "This is a message with an image.", Image = "~/Content/Images/logo.png", Type="journal"}
+
+            };
+            return View(new CubeViewModel() { JournalEntries = cubes });
+        }
+
+        public ActionResult GetCubes()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ SugarCube/Models/JournalEntry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SugarCube.Models
+{
+    public class JournalEntry
+    {
+        public DateTime Date { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public string Image { get; set; }
+        public string Type { get; set; }
+    }
+}
